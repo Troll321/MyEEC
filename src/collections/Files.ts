68f3b217213b89ef363c8b40a3af1cf8f97eb520
Troll321 <atila.ghulwani@gmail.com>
2026-02-05 08:59:18 +0700

    Add Admin description

--- src/collections/Files.ts
+++ src/collections/Files.ts
@@ -2,7 +2,7 @@ import type { CollectionConfig } from "payload"
 
 export const Files: CollectionConfig = {
   slug: "files",
-  admin: {},
+  admin: { description: "Maximum File Size 4.5 MB" },
   upload: { crop: false, focalPoint: false },
   fields: [{ name: "alt", type: "text" }],
 }
